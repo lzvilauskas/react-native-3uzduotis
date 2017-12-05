1b49f48f1b506fbac73bed4f47b0d778d2b9b106
Lukas <zvilauskas.lukas@gmail.com>
2017-12-05 10:11:34 +0200

    Sukuriau pirmo lango komponentus ir stiliu

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View, StyleSheet} from 'react-native';
+import { AppRegistry, Text, View, StyleSheet, TextInput, TouchableHighlight} from 'react-native';
 
 
 export default class TreciaUzduotis extends Component {
@@ -7,10 +7,21 @@ export default class TreciaUzduotis extends Component {
   constructor(){
     super();
     this.state = {
+      pavadinimoTekstas: "",
+      priminimoTekstas:""
     }
  
   }
 
+  onPressAdd()
+  {
+
+  }
+
+  onPressVisi()
+  {
+
+  }
 
   render() {
 
@@ -19,7 +30,28 @@ export default class TreciaUzduotis extends Component {
           
        <View >
 
-     
+       <TextInput
+          style={{height: 40, textAlign:'center'}}
+          placeholder="Iveskite priminimo pavadinima"
+          onChangeText={(tekstas) => this.setState({pavadinimoTekstas: tekstas})}
+        /> 
+        <TextInput
+          style={{ textAlign:'center'}}
+          placeholder="Iveskite norima priminima"
+          onChangeText={(tekstas) => this.setState({priminimoTekstas: tekstas})}
+        />
+
+        <TouchableHighlight onPress={this.onPressAdd.bind(this)}>
+          <View style={stilius.mygtukas}>
+            <Text style={stilius.mygtukasTekstas}>Issaugoti priminima</Text>
+          </View>
+        </TouchableHighlight>
+       
+        <TouchableHighlight onPress={this.onPressVisi.bind(this)}>
+          <View style={stilius.mygtukas}>
+            <Text style={stilius.mygtukasTekstas}>Visi priminimai</Text>
+          </View>
+        </TouchableHighlight>
 
        </View>
        
@@ -31,8 +63,17 @@ export default class TreciaUzduotis extends Component {
 
 const stilius = StyleSheet.create({
   containeris: {
-    flex: 1, 
-  }
+    flex: 1
+  },
+    mygtukas: {
+      marginTop: 20,
+      alignItems: 'center',
+      backgroundColor: "#7FFFD4"
+    },
+    mygtukasTekstas: {
+      padding: 10,
+      color: 'black'
+    }
 })
 
 
