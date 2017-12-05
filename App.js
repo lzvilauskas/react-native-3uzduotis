f3ea3bdbfa0e3fca41e634c523e301bc6554281a
Lukas <zvilauskas.lukas@gmail.com>
2017-12-05 10:23:04 +0200

    Sukuriau duomenu issaugojimo metoda ir pridejau Alert komponenta

--- App.js
+++ App.js
@@ -1,23 +1,62 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View, StyleSheet, TextInput, TouchableHighlight, AsyncStorage} from 'react-native';
+import { AppRegistry, Text, View, StyleSheet, TextInput, TouchableHighlight, AsyncStorage, Alert} from 'react-native';
 
 
 export default class TreciaUzduotis extends Component {
  
   constructor(){
     super();
-    AsyncStorage.setItem('skaicius', '0');
-    AsyncStorage.setItem('visiPriminimai', ' ');
+    //AsyncStorage.setItem('skaicius', '0');
+   //AsyncStorage.setItem('visiPriminimai', ' ');
     this.state = {
       pavadinimoTekstas: "",
-      priminimoTekstas:""
+      priminimoTekstas:"",
+      count: 0
     }
  
   }
 
   onPressAdd()
   {
-
+    if(this.state.pavadinimoTekstas == "" || this.state.priminimoTekstas == "") return;
+    
+  
+     addData = async () => {
+      try{
+      let txt = await AsyncStorage.getItem('skaicius');
+      let sk = parseInt(txt) + 1;
+      this.setState({count: sk});
+      AsyncStorage.setItem('skaicius', sk.toString());
+  
+     
+      let ob = {
+        pavadinimas: this.state.pavadinimoTekstas,
+        priminimas: this.state.priminimoTekstas,
+        }
+     AsyncStorage.setItem("Priminimas" + this.state.count, JSON.stringify(ob));
+  
+     
+     let visi = await AsyncStorage.getItem('visiPriminimai');
+      if (visi == " ")
+      {
+        AsyncStorage.setItem("visiPriminimai", "Priminimas" + this.state.count + ",");
+      }
+      else
+      {
+        let visi2 = await AsyncStorage.getItem('visiPriminimai');
+        let visiPat = visi2 + "Priminimas" + this.state.count + ",";
+        AsyncStorage.setItem("visiPriminimai", visiPat);
+        //alert(visiPat);
+      } 
+    }
+      catch(error)
+      {
+        alert(error);
+      }
+    }
+    addData();   
+    
+    Alert.alert("Pranesimas","Priminimas issaugotas");
   }
 
   onPressVisi()
