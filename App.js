6362777a0f0c12f0ce822cdc3a16c1a889541aa3
Lukas <zvilauskas.lukas@gmail.com>
2017-12-05 09:58:19 +0200

    Pradinis kodas

---
+++ App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { AppRegistry, Text, View, StyleSheet} from 'react-native';
+
+
+export default class TreciaUzduotis extends Component {
+ 
+  constructor(){
+    super();
+    this.state = {
+    }
+ 
+  }
+
+
+  render() {
+
+    return (
+    <View >
+          
+       <View >
+
+     
+
+       </View>
+       
+   </View>  
+    );
+  }
+}
+
+
+const stilius = StyleSheet.create({
+  containeris: {
+    flex: 1, 
+  }
+})
+
+
